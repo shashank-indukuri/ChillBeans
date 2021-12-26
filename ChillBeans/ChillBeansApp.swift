bc5bc54232fca9d19378ada3e3837f00a1ec20a6
shashank-indukuri <shashank.indukuri05@gmail.com>
2021-12-26 15:30:21 -0500

    Initial Commit

---
+++ ChillBeans/ChillBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChillBeansApp.swift
+//  ChillBeans
+//
+//  Created by Shashank Indukuri on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct ChillBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
