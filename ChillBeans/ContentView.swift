f6e3f9afabacf1a4634565fd76a4db3c235f923d
shashank-indukuri <shashank.indukuri05@gmail.com>
2021-12-28 16:35:10 -0500

    Add color to swipe action

--- ChillBeans/ContentView.swift
+++ ChillBeans/ContentView.swift
@@ -59,6 +59,7 @@ struct ContentView: View {
                             } label: {
                                 Label("Reorder", systemImage: "repeat")
                             }
+                            .tint(.blue)
                         }
                     }
                 }
